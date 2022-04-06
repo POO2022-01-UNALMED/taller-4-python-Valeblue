445de198338cbe20de8cacb8837940eeb19ca6f0
Valeblue <101608411+Valeblue@users.noreply.github.com>
2022-04-05 23:50:01 -0500

    :100:

--- classroom/asignatura.py
+++ classroom/asignatura.py
@@ -3,7 +3,10 @@ class Asignatura:
     def __init__(self, nombre, salon="remoto"):
         self._nombre = nombre
         self._salon = salon
-     def __str__(self):
-        return self._nombre+" "+self.
+    def __str__(self):
+        return self._nombre+" "+self._salon
         
+
+
+
 
